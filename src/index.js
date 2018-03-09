a634c8fa7ca21cfa942eff1db5d7191f9db85ae8
Afei Zhang <afei.zhang@boldseas.com>
2018-03-09 19:05:41 +0800

    dsf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './searchPage/searchPage';
 import NavbarHome from './navbarHome/navbarHome'
 import registerServiceWorker from './registerServiceWorker';
 
